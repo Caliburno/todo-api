2e1ee8307cbadf6485e331bcc6ed755cc5fe5cc3
Caliburno <dario.buffa@gmail.com>
2026-01-11 20:11:03 -0300

    Initial commit - Spring Boot TODO API

---
+++ todo-api/src/test/java/io/github/Caliburno/todo_api/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Caliburno.todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
